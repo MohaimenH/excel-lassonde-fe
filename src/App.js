3ba1dad9d26671fcaf78fd54377d1d8d23fcf5a5
MohaimenH <mohaimenhassan08@gmail.com>
2022-02-05 20:41:24 -0500

    Setup React and Tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="flex flex-col justify-center items-center h-screen bg-gray-800">
+            <div className="mx-auto max-w-3xl p-4 shadow-xl rounded bg-slate-100">
+                <div className="text-center">
+                    <img src={logo} alt="Logo" className="h-32 my-4 mx-auto" />
+
+                    <div className="text-gray-500 text-2xl font-bold">
+                        <h1>Welcome to Tailwind!</h1>
+                    </div>
+
+                    <div className="my-4">
+                        <p>
+                            Learn more by{" "}
+                            <a
+                                href="https://tailwindcss.com/docs/utility-first"
+                                className="text-blue-400"
+                            >
+                                visiting the docs.
+                            </a>
+                        </p>
+                        <p>
+                            Cheers!
+                            <span role="img" aria-labelledby="cheers emoji">
+                                🎊
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
