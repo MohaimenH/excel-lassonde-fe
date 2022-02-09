70992e14296502b95d9fcfa963743f206cee0112
MohaimenH <mohaimenhassan08@gmail.com>
2022-02-08 19:34:44 -0500

    Fixed CORS and component logic

--- src/components/EventsCollection.js
+++ src/components/EventsCollection.js
@@ -7,21 +7,29 @@ const EventsCollection = (props) => {
   const [events, setEvents] = useState([
     { title: "", description: "", date_start: "", location: "", imgUrl: "" },
   ]);
+  let [loading, setLoading] = useState(false);
 
   useEffect(() => {
     axios.get("https://excellassonde.xyz/events").then((res) => {
       setEvents(res.data);
+      setLoading(true);
     });
   }, []);
 
   return (
-    <ul className="flex flex-row justify">
-      {events.map((e, index) => (
-        <li key={index} className="m-5">
-          <EventCard {...e} />
-        </li>
-      ))}
-    </ul>
+    <>
+      {loading ? (
+        <ul className="flex flex-row justify">
+          {events.map((e, index) => (
+            <li key={index} className="m-5">
+              <EventCard {...e} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div></div>
+      )}
+    </>
   );
 };
 
